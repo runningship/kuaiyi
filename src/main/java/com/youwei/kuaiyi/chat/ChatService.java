abe4a28cb21d7547341731c784a0e5111ec0a68e
runningship <runningship@gmail.com>
2015-11-16 23:39:46 +0800

    more

--- src/main/java/com/youwei/kuaiyi/chat/ChatService.java
+++ src/main/java/com/youwei/kuaiyi/chat/ChatService.java
@@ -1,82 +1,41 @@
 package com.youwei.kuaiyi.chat;
 
+import java.util.List;
+
 import org.bc.sdak.CommonDaoService;
 import org.bc.sdak.TransactionalServiceHelper;
 import org.bc.web.ModelAndView;
 import org.bc.web.Module;
 import org.bc.web.WebMethod;
 
-import com.youwei.kuaiyi.entity.Article;
 
-
-@Module(name="/admin/chat")
+@Module(name="/admin/zone")
 public class ChatService {
 
 	CommonDaoService dao = TransactionalServiceHelper.getTransactionalService(CommonDaoService.class);
 
 	@WebMethod
-	public ModelAndView sendSingleChatMsg(Integer myId , Integer buddyId , String msg){
+	public ModelAndView publishShuoShuo(Integer myId , List<String> imageList , String msg){
 		ModelAndView mv = new ModelAndView();
 		return mv;
 	}
 	
 	@WebMethod
-	public ModelAndView sendGroupChatMsg(Integer myId , Integer groupId , String msg){
+	public ModelAndView loadShuoShuo(Integer myId , Integer currentPage){
 		ModelAndView mv = new ModelAndView();
 		return mv;
 	}
 	
-	@WebMethod
-	public ModelAndView emptSingleChatMsg(Integer myId , Integer buddyId){
-		ModelAndView mv = new ModelAndView();
-		return mv;
+	private String getMyZoneDir(Integer myId){
+		//host+disk+dir
+		return "";
 	}
 	
-	@WebMethod
-	public ModelAndView emptyGroupChatMsg(Integer myId , Integer groupId){
-		ModelAndView mv = new ModelAndView();
-		return mv;
+	private String getHost(Integer uid){
+		return "";
 	}
 	
-	@WebMethod
-	public ModelAndView openSingleChat(Integer myId , Integer buddyId){
-		ModelAndView mv = new ModelAndView();
-		return mv;
-	}
-	
-	@WebMethod
-	public ModelAndView openGroupChat(Integer myId , Integer groupId){
-		ModelAndView mv = new ModelAndView();
-		return mv;
-	}
-	
-	@WebMethod
-	public ModelAndView inviteContact(Integer buddyId){
-		ModelAndView mv = new ModelAndView();
-		return mv;
-	}
-	
-	@WebMethod
-	public ModelAndView acceptInvitation(Integer buddyId){
-		ModelAndView mv = new ModelAndView();
-		return mv;
-	}
-	
-	@WebMethod
-	public ModelAndView addBuddyToGroup(Integer buddyId , Integer groupId){
-		ModelAndView mv = new ModelAndView();
-		return mv;
-	}
-	
-	@WebMethod
-	public ModelAndView removeBuddy(Integer buddyId){
-		ModelAndView mv = new ModelAndView();
-		return mv;
-	}
-	
-	@WebMethod
-	public ModelAndView quitGroup(Integer groupId){
-		ModelAndView mv = new ModelAndView();
-		return mv;
+	private String getDir(Integer uid){
+		return "";
 	}
 }
