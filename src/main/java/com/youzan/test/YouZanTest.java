6a0ddd70f88a691232f060004caad09d3a54753e
runningship <runningship@gmail.com>
2016-01-03 20:46:24 +0800

    youzan test

---
+++ src/main/java/com/youzan/test/YouZanTest.java
@@ -0,0 +1,115 @@
+package com.youzan.test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+
+public class YouZanTest {
+
+	private static final String APP_ID = "79066c310840000808"; //这里换成你的app_id
+	private static final String APP_SECRET = "c75043f101d50457ed2eda77a446329c"; //这里换成你的app_secret
+	
+	public static void main(String[] args){
+		//sendGet();
+		//sendPost();
+		getFoodsList();
+	}
+	
+	private static void getFoodsList(){
+		//kdt.items.onsale.get
+		String method = "kdt.items.onsale.get";
+		HashMap<String, String> params = new HashMap<String, String>();
+//		params.put("num_iid", "2651514");
+		
+		KdtApiClient kdtApiClient;
+		HttpResponse response;
+		
+		try {
+			kdtApiClient = new KdtApiClient(APP_ID, APP_SECRET);
+			response = kdtApiClient.get(method, params);
+			System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			StringBuffer result = new StringBuffer();
+			String line = "";
+			while ((line = bufferedReader.readLine()) != null) {
+				result.append(line);
+			}
+
+			System.out.println(result.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * 测试获取单个商品信息
+	 */
+	private static void sendGet(){
+		String method = "kdt.item.get";
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("num_iid", "2651514");
+		
+		KdtApiClient kdtApiClient;
+		HttpResponse response;
+		
+		try {
+			kdtApiClient = new KdtApiClient(APP_ID, APP_SECRET);
+			response = kdtApiClient.get(method, params);
+			System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			StringBuffer result = new StringBuffer();
+			String line = "";
+			while ((line = bufferedReader.readLine()) != null) {
+				result.append(line);
+			}
+
+			System.out.println(result.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * 测试获取添加商品
+	 */
+	private static void sendPost(){
+		String method = "kdt.item.add";
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("price", "999.01");
+		params.put("title", "测试商品");
+		params.put("desc", "这是一个号商铺");
+		params.put("is_virtual", "0");
+		params.put("post_fee", "10.01");
+		params.put("sku_properties", "");
+		params.put("sku_quantities", "");
+		params.put("sku_prices", "");
+		params.put("sku_outer_ids", "");
+		String fileKey = "images[]";
+		List<String> filePaths = new ArrayList<String>();
+		filePaths.add("/Users/xuexiaozhe/Desktop/1.png");
+		filePaths.add("/Users/xuexiaozhe/Desktop/2.png");
+		
+		KdtApiClient kdtApiClient;
+		HttpResponse response;
+		
+		try {
+			kdtApiClient = new KdtApiClient(APP_ID, APP_SECRET);
+			response = kdtApiClient.post(method, params, filePaths, fileKey);
+			System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			StringBuffer result = new StringBuffer();
+			String line = "";
+			while ((line = bufferedReader.readLine()) != null) {
+				result.append(line);
+			}
+
+			System.out.println(result.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
